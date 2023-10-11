98b5606dfb351f3fb1c9303d3eee3dd2db644794
andrey123Ac <139496165+andrey123Ac@users.noreply.github.com>
2023-10-11 16:51:09 -0300

    Add files via upload

---
+++ js/arquivo.js
@@ -0,0 +1,107 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const botaoStar = document.querySelector('.botaoStar')
+const reiciniar = document.querySelector('.reiniciar')
+const pontos = document.querySelector('.pontos');
+const areaPontos = document.querySelector('.areaPontos');
+
+
+const jump = () => {
+
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump');
+
+    }, 500);
+
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ' ');
+
+    if (pipePosition < 120 && pipePosition > 0 && marioPosition < 80) {
+
+        pipe.style.animation = 'nome'
+        pipe.classList.add('left');
+
+
+        mario.style.animation = 'nome'
+        mario.style.bottom = `${marioPosition}px`
+        mario.src = 'img/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+        reiciniar.classList.remove('hide');
+        pontos.innerHTML = ''
+        contador = 0
+        som.pause()
+
+
+
+        reiciniar.addEventListener('click', () => {
+
+            pipe.classList.remove('left');
+            pipe.style.removeProperty("animation");
+
+            mario.style.removeProperty("animation");
+            mario.src = 'img/mario.gif';
+            mario.style.width = '150px'
+            mario.style.bottom = 0
+            som.play()
+            setTimeout(() => {
+
+                reiciniar.classList.add('hide');
+
+            }, 500);
+
+        });
+
+
+    } else {
+
+    }
+
+
+
+
+
+
+});
+
+botaoStar.addEventListener('click', () => {
+
+    pipe.classList.remove('posicaoInicialcano');
+    pipe.classList.add('inimacaoCano');
+    botaoStar.classList.add('hide');
+    contador = 0
+    pontos.innerHTML = "";
+  som = new Audio('audio_theme.mp3')
+  som.play()
+   
+});
+
+
+document.addEventListener('keydown', () => {
+    jump()
+    aumentarContador()
+
+});
+
+// Inicializa o contador
+let contador = 0;
+
+function aumentarContador() {
+    contador++;
+    atualizarContador();
+}
+
+
+function atualizarContador() {
+
+    pontos.textContent = contador;
+}
+
+
